d3ce508042252717399d4f8a6494a50ba4818a0f
Antonio <eltony162@gmail.com>
2017-02-01 12:44:27 -0600

    Factory Pattern
    C# Factory Pattern Example

---
+++ FactoryPatternExample/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace FactoryPatternExample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
